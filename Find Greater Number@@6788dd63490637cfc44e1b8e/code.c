801d44a6af39bbef2ca89565c2ce17937fec4123
simrnn-034 <simrn.0344@gmail.com>
2025-01-29 12:52:22 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a>b){
+        printf("%d",a);
+    }
+    else if(a<b){
+        printf("%d",b);
+    }
+    else{
+        printf("Invalid");
+    }
+    return 0;
+}
