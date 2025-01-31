d97ed6e083e78dd923f54dc4edef26ff0a7fa8e7
simrnn-034 <simrn.0344@gmail.com>
2025-01-31 10:13:20 +0530

    Solution to Find Day of the Week

--- Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -22,6 +22,9 @@ int main() {
         case 6:
             printf("Saturday");
             break;   
+        case 7:
+            printf("Sunday");
+            break;    
         default:
             printf("Invalid"); 
             break;        
