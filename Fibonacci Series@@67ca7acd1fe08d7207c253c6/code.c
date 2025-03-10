464f4cb416b4428d04e84e0c7adf85170564b5c9
simrnn-034 <simrn.0344@gmail.com>
2025-03-10 11:25:59 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,10 @@
-// Your code here...
+// Your code here..
+.
 int fibonacciSeries(n){
     if(n==1){
+        return 0;
+    }
+    if(n==2){
         return 1;
     }
     return fibonacciSeries(n-1)+fibonacciSeries(n-2);
