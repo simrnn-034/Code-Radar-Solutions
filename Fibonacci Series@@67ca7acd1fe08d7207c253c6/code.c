2a305a6c7fadb014b6bba4e0b32dba667df1ee36
simrnn-034 <simrn.0344@gmail.com>
2025-03-12 11:15:24 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,14 +1,20 @@
 // Your code here..
 
-int fibonacciSeries(int n){
+int fibonacci(int n){
     if(n==1){
-         printf("0");
+         return 0;
     }
     if(n==2){
-        printf("1");
+        return 1;
     }
     for(int i=0;i<n;i++){
-        printf("%d", fibonacciSeries(n-1)+fibonacciSeries(n-2));
+        return fibonacci(n-1)+fibonacci(n-2);
     }
-    return 0;
-  }
+  
+}
+void fibonacciSeries(int n){
+    for(int i=1;i<=n;i++){
+        printf("%d ",fibonacci(i));
+    }
+    
+}
