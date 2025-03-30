84a09171ab62bc9af0b96f5408f3ac894b80c2d7
simrnn-034 <simrn.0344@gmail.com>
2025-03-31 00:03:18 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -1,34 +1,51 @@
-// Your code here...
-# include <stdio.h>
-void primecheck(int arr[],int n);
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-    }
-    primecheck(arr,n);
+#include <stdio.h>
 
+void primecheck(int arr[], int n);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Check for prime numbers
+    primecheck(arr, n);
+
+    return 0;
 }
-void primecheck(int arr[],int n){
-    int count_not=0;
-    int count=0;
-    for(int i=0;i<n;i++){
-        int num=arr[i];
-        int isPrime=1;
-        if(num<=1){
-            isPrime=0;
-        }
-        for(int j=2;j*j<=i;j++){
-            if(num%j==0){
-                isPrime=0;                
+
+// Function to count prime numbers in the array
+void primecheck(int arr[], int n) {
+    int count = 0; // Count of prime numbers
+
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];  // Get the current number
+        int isPrime = 1;   // Assume prime initially
+
+        // Handle numbers less than or equal to 1
+        if (num <= 1) {
+            isPrime = 0; // 0 and 1 are not prime
+        } 
+        // Check divisibility only for numbers greater than 1
+        else {
+            for (int j = 2; j * j <= num; j++) {
+                if (num % j == 0) {
+                    isPrime = 0; // Not prime if divisible
+                    break;
+                }
             }
-            
         }
-        if(isPrime){
+
+        // Increment count if the number is prime
+        if (isPrime) {
             count++;
         }
     }
-    printf("%d",count);
+
+    // Print the total number of prime numbers
+    printf("%d\n", count);
 }
