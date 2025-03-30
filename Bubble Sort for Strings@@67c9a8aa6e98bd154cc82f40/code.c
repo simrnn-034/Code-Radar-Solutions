957335684d7208cb7d731cf164e576593b5b4903
simrnn-034 <simrn.0344@gmail.com>
2025-03-30 17:27:33 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,9 +1,9 @@
 // Your code here...
 void bubbleSort(char arr[][100],int n){
     char temp[100];
-    for(int i=0;i<n;++i){
+    for(int i=0;i<n-1;++i){
         for(int j=0;j<n-i-1;++j){
-            if(strcmp(arr[j],arr[j+1]>0)){
+            if(strcmp(arr[j],arr[j+1])>0){
                 strcpy(temp,arr[j]);
                 strcpy(arr[j],arr[j+1]);
                 strcpy(arr[j+1],temp);
